dc2c911cdb07d5c26f8544fc7e57e94159b4da14
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-14 20:58:21 +0000

    Complete progamming_languages lab

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,24 @@
 def reformat_languages(languages)
-  # your code here
+ oo_hash = {}
+  functional_hash = {}
+
+  total_keys = languages[:oo].keys + languages[:functional].keys
+
+  dup_langs = total_keys.select{|language| total_keys.count(language) > 1}
+
+  languages[:oo].each do |key, values|
+    values.each{|k, v|oo_hash[key] = {:type => v, :style => [:oo]}}
+  end
+
+  languages[:functional].each do |key, values|
+    values.each{|k, v|functional_hash[key] = {:type => v, :style => [:functional]}}
+  end
+
+  new_hash = functional_hash.merge(oo_hash)
+
+  new_hash.each do |keys, values|
+    new_hash[keys][:style] += [:functional] if dup_langs.include?(keys)
+  end
+
+new_hash
 end
