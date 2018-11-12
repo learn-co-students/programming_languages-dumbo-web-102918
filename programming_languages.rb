acd3add8b35b60477bfa708af9e6230b6c823169
a lan <bluenowte97@gmail.com>
2018-11-12 20:14:54 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+  reformatted = {}
+  intermediate = languages.map do |key, value|
+    value.each do |name, info|
+    reformatted[name] = info
+    reformatted[name][:style] = []
+    array = reformatted[name][:style]
+    array.push(key)
+     #value[:style].push(intermediate[key])
+  end
+end
+reformatted[:javascript][:style].unshift(:oo)
+reformatted
 end
